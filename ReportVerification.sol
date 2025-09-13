2cc7cb5c6be4606947c25ec9dd0814f1690884f9
Sayan Senapati <sayansenapati2544@gmail.com>
2025-09-14 00:50:20 +0530

    adding api/storeReport and api/verifyReport

--- ReportVerification.sol
+++ ReportVerification.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SeoVerification {
+contract ReportVerification {
     mapping(string => uint256) private reportTimestamps;
     
     event ReportStored(string indexed reportHash, uint256 timestamp);
